8a82d846fb8cec1f5864d25d97a8571ab3fff5f1
sevenbean <50545408+sevenbean@users.noreply.github.com>
2020-08-28 15:50:07 +0800

    Delete test1.py

--- TensorFlow的API使用/tfrcoder的API使用/test1.py
+++
@@ -1,7 +0,0 @@
-#tfrecoder文件格式的使用
-import tensorflow as tf
-favo_books=[name.encode("utf8") for name in ["machine learning","cc150"]]
-favo_books_bytelist=tf.train.BytesList(value=favo_books)
-print(favo_books_bytelist)
-favo_books_Floatlist=tf.train.FloatList(value=favo_books)
-print(favo_books_Floatlist)
